9374a9ba0747efac112a3e129cfd388ac80d2df4
Kimball Johnson <kimball.johnson@engagile.com>
2017-09-22 22:50:22 -0700

    Initial commit
    added all original files from azure mobile app client

---
+++ AzrMblSdkXamFrm2/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AzrMblSdkXamFrm2
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://azrmblsdkxamfrm2.azurewebsites.net";
+	}
+}
+
